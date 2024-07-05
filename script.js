82ac93ba4296ca1d19ffb2c7d44d5a022fef5848
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2024-07-05 06:04:31 +0300

    Complete the UI
    code still needs to be cleaned and refactored

--- script.js
+++ script.js
@@ -47,45 +47,56 @@ function playRound(humanChoice, computerChoice) {
     } 
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        let result = playRound(humanSelection, computerSelection);
+//     for (let i = 0; i < 5; i++) {
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(humanSelection, computerSelection);
 
-        if (result === WIN_MESSAGE) {
-            humanScore++;
-        }
-        else if (result === LOSE_MESSAGE) {
-            computerScore++;
-        }
-    }
+//         if (result === WIN_MESSAGE) {
+//             humanScore++;
+//         }
+//         else if (result === LOSE_MESSAGE) {
+//             computerScore++;
+//         }
+//     }
 
-    alert("Your score is " + humanScore);
-    alert("Computer score is " + computerScore);
+//     alert("Your score is " + humanScore);
+//     alert("Computer score is " + computerScore);
 
-    if (humanScore > computerScore) {
-        alert("You won over the Computer");
-    }
-    else if (humanScore < computerScore) {
-        alert("Computer won");
-    }
-    else {
-        alert("a tie");
-    }
-}
-let playerScore = 0;
-let computerScore = 0;
+//     if (humanScore > computerScore) {
+//         alert("You won over the Computer");
+//     }
+//     else if (humanScore < computerScore) {
+//         alert("Computer won");
+//     }
+//     else {
+//         alert("a tie");
+//     }
+// }
+const round = document.querySelector("#round");
 const game = document.querySelector("#game");
 const score = document.querySelector("#score");
 const playerResult = document.querySelector("#player-score");
 const computerResult = document.querySelector("#computer-score");
-game.addEventListener("click", e => {
+const playAgain = document.querySelector("#play-again");
+const result = document.querySelector("#result");
+let playerScore = 0;
+let computerScore = 0;
+let counter = 1;
+
+function playGame(e) {
+    // ignore click on the parent div#game
+    if (e.target.id === "game") {
+        return;
+    }
     const computerSelection = getComputerChoice()
     const roundResult = playRound(e.target.id, computerSelection);
+
+    round.textContent = `Round ${counter + 1}/5`;
     if (roundResult === WIN_MESSAGE) {
         playerScore++;
         playerResult.textContent = `Your Score: ${playerScore}`;
@@ -99,4 +110,40 @@ game.addEventListener("click", e => {
     else {
         result.textContent = "a tie";
     }
+    if (counter === 5) {
+        showWinner();
+        playerScore = 0;
+        computerScore = 0;
+        counter = 1;
+        round.textContent = `Round 5/5`;
+        game.removeEventListener("click", playGame);
+        playAgain.style.display = "block";
+        return;
+    }
+    counter++;
+}
+
+game.addEventListener("click", playGame);
+playAgain.addEventListener("click", e => {
+    document.querySelector("#winner").remove();
+    round.textContent = `Round 1/5`;
+    playerResult.textContent = "Your Score: 0";
+    computerResult.textContent = "Computer Score: 0";
+    result.textContent = "choose one to start the game";
+    game.addEventListener("click", playGame);
+    playAgain.style.display = "none";
 })
+function showWinner() {
+    const winner = document.createElement("div");
+    winner.id = "winner";
+    if (playerScore > computerScore) {
+        winner.textContent = "You won over the computer";
+    }
+    else if (playerScore < computerScore) {
+        winner.textContent = "You lost against the Computer";
+    }
+    else {
+        winner.textContent = "A Tie";
+    }
+    document.body.insertBefore(winner, playAgain);
+}
