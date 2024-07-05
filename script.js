45923494588f509c3ffd4fdb1fa970cc5ea9a9c5
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2024-07-05 17:34:07 +0300

    Clean code and remove unused functions

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ const SCISSORS = "scissors";
 const WIN_MESSAGE = "You WON !!";
 const LOSE_MESSAGE = "You lost :(";
 const TIE_MESSAGE = "a tie";
+const round = document.querySelector("#round");
+const game = document.querySelector("#game");
+const score = document.querySelector("#score");
+const playerResult = document.querySelector("#player-score");
+const computerResult = document.querySelector("#computer-score");
+const playAgain = document.querySelector("#play-again");
+const result = document.querySelector("#result");
 
-function getHumanChoice() {
-    while (true) {
-        let choice = prompt("Enter your choice").toLowerCase();
-        if (choice === ROCK || choice === PAPER || choice === SCISSORS) {
-            return choice;
-        }
-        else {
-            alert("Invalid input. Please enter rock, paper, or scissors.");
-        }
-    }
-}
+let playerScore = 0;
+let computerScore = 0;
+let counter = 1;
 
 function getComputerChoice() {
     const randomNumber = Math.random();
@@ -30,9 +29,7 @@ function getComputerChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-
-        
+function playRound(humanChoice, computerChoice) {   
     if (humanChoice === computerChoice) {
         return TIE_MESSAGE;
     }
@@ -47,46 +44,20 @@ function playRound(humanChoice, computerChoice) {
     } 
 }
 
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-
-//     for (let i = 0; i < 5; i++) {
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
-//         let result = playRound(humanSelection, computerSelection);
-
-//         if (result === WIN_MESSAGE) {
-//             humanScore++;
-//         }
-//         else if (result === LOSE_MESSAGE) {
-//             computerScore++;
-//         }
-//     }
-
-//     alert("Your score is " + humanScore);
-//     alert("Computer score is " + computerScore);
-
-//     if (humanScore > computerScore) {
-//         alert("You won over the Computer");
-//     }
-//     else if (humanScore < computerScore) {
-//         alert("Computer won");
-//     }
-//     else {
-//         alert("a tie");
-//     }
-// }
-const round = document.querySelector("#round");
-const game = document.querySelector("#game");
-const score = document.querySelector("#score");
-const playerResult = document.querySelector("#player-score");
-const computerResult = document.querySelector("#computer-score");
-const playAgain = document.querySelector("#play-again");
-const result = document.querySelector("#result");
-let playerScore = 0;
-let computerScore = 0;
-let counter = 1;
+function showWinner() {
+    const winner = document.createElement("div");
+    winner.id = "winner";
+    if (playerScore > computerScore) {
+        winner.textContent = "You won over the computer";
+    }
+    else if (playerScore < computerScore) {
+        winner.textContent = "You lost against the Computer";
+    }
+    else {
+        winner.textContent = "A Tie";
+    }
+    document.body.insertBefore(winner, playAgain);
+}
 
 function playGame(e) {
     // ignore click on the parent div#game
@@ -104,18 +75,19 @@ function playGame(e) {
     }
     else if (roundResult === LOSE_MESSAGE) {
         computerScore++;
-        result.textContent = roundResult + ` ${computerSelection} beats ${e.target.id}`;
         computerResult.textContent = `Computer Score: ${computerScore}`;
+        result.textContent = roundResult + ` ${computerSelection} beats ${e.target.id}`;
     }
     else {
         result.textContent = "a tie";
     }
+
     if (counter === 5) {
         showWinner();
         playerScore = 0;
         computerScore = 0;
         counter = 1;
-        round.textContent = `Round 5/5`;
+        round.textContent = `Round 5/5`; // don't update the counter to 6 keep it at 5
         game.removeEventListener("click", playGame);
         playAgain.style.display = "block";
         return;
@@ -124,6 +96,7 @@ function playGame(e) {
 }
 
 game.addEventListener("click", playGame);
+
 playAgain.addEventListener("click", e => {
     document.querySelector("#winner").remove();
     round.textContent = `Round 1/5`;
@@ -132,18 +105,4 @@ playAgain.addEventListener("click", e => {
     result.textContent = "choose one to start the game";
     game.addEventListener("click", playGame);
     playAgain.style.display = "none";
-})
-function showWinner() {
-    const winner = document.createElement("div");
-    winner.id = "winner";
-    if (playerScore > computerScore) {
-        winner.textContent = "You won over the computer";
-    }
-    else if (playerScore < computerScore) {
-        winner.textContent = "You lost against the Computer";
-    }
-    else {
-        winner.textContent = "A Tie";
-    }
-    document.body.insertBefore(winner, playAgain);
-}
+});
